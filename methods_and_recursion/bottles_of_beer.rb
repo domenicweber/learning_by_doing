f42b48b93c23e57e488b6eb3815711945739821f
Domenic Weber <domenic.weber@gmail.com>
2015-06-10 10:06:30 -0600

    Initial commit adds bottles_of_beer.rb

---
+++ methods_and_recursion/bottles_of_beer.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+def bottle(number)
+  puts "#{number} bottles of beer on the wall"
+  puts "#{number} bottles of beer. Take one down pass it around,"
+  number -= 1
+  puts "#{number} of beer on the wall"
+
+  if number > 0
+    bottle(number)
+  else number == 0
+  puts "#{number} bottles of beer. Take one down pass it around,"
+  puts "#{number} of beer on the wall"
+  end
+end
+
+bottle(99)
