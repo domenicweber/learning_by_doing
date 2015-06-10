3b6a9d9aadc8a3b8abc146e3a465d76202864584
Domenic Weber <domenic.weber@gmail.com>
2015-06-10 14:26:25 -0600

    Adjusts lines for lyrics
    [#96677314]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
 def bottle(number)
-  puts "#{number} bottles of beer on the wall"
-  puts "#{number} bottles of beer. Take one down pass it around,"
+  puts "#{number} bottles of beer on the wall, #{number} bottles of beer."
+  puts "Take one down pass it around,"
   number -= 1
   puts "#{number} of beer on the wall"
   puts ''
