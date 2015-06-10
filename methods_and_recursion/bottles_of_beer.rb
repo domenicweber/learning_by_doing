518d6943e058cdd783e1f7a62861c8a8aaf63143
Domenic Weber <domenic.weber@gmail.com>
2015-06-10 16:16:16 -0600

    Adds what I hope are finial lines....
    [#96677314]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -26,6 +26,7 @@ while number == 2
 end
 
 puts "No more bottles of beer on the wall, no more bottles of beer."
+puts "Go to the store and buy some more, 99 bottles of beer on the wall."
 
 
 # if number == 1
