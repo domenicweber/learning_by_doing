d6d99ec1bdff9e5876e07c5dbf3be886c6822b42
Domenic Weber <domenic.weber@gmail.com>
2015-06-12 08:29:43 -0600

    Updates variable definitions to more confusion verbiage
    In an attempt to make things clearer I've upadated all the variable
    definitons to use more words, making it tedious and more difficult to
    read and understand to people outside of the coding industry because
    evidently the desire of coders is to make sure no one understands what
    the are actually saying when they attempt to communicate with people.
    But hey, if that's what my superiors want, that's what I'll do.
    
    [#96449482]

--- numbers_letters_and_variables/number_of_seconds.rb
+++ numbers_letters_and_variables/number_of_seconds.rb
@@ -2,28 +2,28 @@
 # 1 second is defined as 1 second
 sec = 1
 # 1 min is 60 seconds
-min = 60
+secs_per_min = 60
 # 1 hour is 60 min * 60 seconds = 3600 seconds
-hour = 3600 / 60
+min_per_hour = 3600 / 60
 # 1 day is defined as 24 hours
-day = "#{hour.to_i / 60 * 24}"
+hours_per_day = "#{(min_per_hour.to_i / 60) * 24}"
 #
-week = "#{day.to_i * 7 / 24}"
+week = "#{hours_per_day.to_i * 7 / 24}"
 # This year accounts for leap year by using 365.25 days/year
-year = "#{day.to_i * 365.25}"
-sec_per_hour = "#{sec.to_i * min.to_i * hour.to_i}"
+year = "#{hours_per_day.to_i * 365.25}"
+sec_per_hour = "#{sec.to_i * secs_per_min.to_i * min_per_hour.to_i}"
 # This tells us there are 60 * 60 * 24 * 7 seconds in a year
-sec_per_week = "#{60 * min * day.to_i * week.to_i}"
+sec_per_week = "#{60 * secs_per_min * hours_per_day.to_i * week.to_i}"
 # Seconds in a year
-wtf = "#{year.to_i * hour.to_i * 60}"
+wtf = "#{year.to_i * min_per_hour.to_i * 60}"
 puts "There are #{sec * 60} seconds in a minute."
-puts "There are #{min} minutes in an hour."
-puts "There are #{day} hours in a day."
+puts "There are #{secs_per_min} minutes in an hour."
+puts "There are #{hours_per_day} hours in a day."
 puts "There are #{week} days in a week."
 puts "That means there are:
   #{sec_per_hour} seconds in an hour,
-  #{60 * min.to_i * day.to_i} seconds in a day,
-  #{60 * min.to_i * day.to_i * week.to_i} seconds in a week"
+  #{60 * secs_per_min.to_i * hours_per_day.to_i} seconds in a day,
+  #{60 * secs_per_min.to_i * hours_per_day.to_i * week.to_i} seconds in a week"
 # sec_per_week variable uses seconds, hours, days and weeks
 # Since years doesn't technically = 365 days or 52
 # weeks I used a different variable to defines seconds per year #{100 * wtf.to_i} but to
