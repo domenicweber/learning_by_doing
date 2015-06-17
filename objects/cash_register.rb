505daff9f2149432b1c50cf2a47114bb1e2cc1d8
Domenic Weber <domenic.weber@gmail.com>
2015-06-17 08:18:04 -0600

    Adds lines 11 and 17 to prove @total changes
    It was said that @total_owed would not change if @total_owed =+
    amount_owed was used to define the purchase method.  To disprove this a
    line was added directly underneath this to the the purchase method that
    shows the new @total_owed when the purchase parameter (amount_owed) is
    set.  It reads as follows: puts "You owe $X.XX" and the X.XX taken from the new
    @total_owed.
    
    [#96738650]

--- objects/cash_register.rb
+++ objects/cash_register.rb
@@ -8,15 +8,17 @@ class CashRegister
   #takes a floating number and adds it to a total
   def purchase(amount_owed)
     @total_owed =+ amount_owed
+    puts "You owe $#{@total_owed}"
   end
 
   #takes one floating number for how much is paid, and
   #should return how much change is given
   def pay(amount_paid)
+    puts "You just paid $#{amount_paid}"
     if amount_paid > @total_owed
       @change = amount_paid - @total_owed
       @total_owed = 0
-      puts "Your change is $#{@change}"
+      puts "Your change is $#{@change.round(2)}"
     elsif amount_paid < @total_owed
       @total_owed = @total_owed - amount_paid
       puts "Your new total is $#{@total_owed}"
@@ -33,11 +35,12 @@ end
 
 x = CashRegister.new
 
-items_purchased = (x.purchase(3) + x.purchase(5))
+items_purchased = x.purchase(3)
 items_paid_for = x.pay(2)
+puts ""
 items_purchased2 = x.purchase(9)
 items_paid_for2 = x.pay(3)
-
+puts ""
 
 if __FILE__ == $0
   register = CashRegister.new
